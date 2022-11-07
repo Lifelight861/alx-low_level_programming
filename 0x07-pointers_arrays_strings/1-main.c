bdfdef94231f5ce74fece9e80ec9676e547e1dff
Lifelight861 <iabiodun861@gmail.com>
2022-11-07 10:39:54 -0800

    life

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 #include <stdio.h>
 
@@ -15,20 +14,13 @@ void simple_print_buffer(char *buffer, unsigned int size)
 
         i = 0;
         while (i < size)
-        {
                 if (i % 10)
-                {
                         printf(" ");
-                }
                 if (!(i % 10) && i)
-                {
                         printf("\n");
-                }
                 printf("0x%02x", buffer[i]);
                 i++;
-        }
         printf("\n");
-}
 
 /**
  * main - check the code
@@ -36,7 +28,6 @@ void simple_print_buffer(char *buffer, unsigned int size)
  * Return: Always 0.
  */
 int main(void)
-{
     char buffer[98] = {0x00};
 
     simple_print_buffer(buffer, 98);
